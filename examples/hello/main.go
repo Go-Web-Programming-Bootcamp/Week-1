4ea4aca4ac1669b4da77a9c195f35ce50c2ef585
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-14 21:47:29 +0700

    Another type of for loop

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -3,16 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 4; i++ {
-		switch i {
-		case 1:
-			fmt.Println("One")
-		case 2:
-			fmt.Println("Two")
-		case 3:
-			fmt.Println("Three")
-		default:
-			fmt.Println("Unknown Number")
-		}
+	for range "test" {
+		fmt.Println("Hello!")
 	}
 }
