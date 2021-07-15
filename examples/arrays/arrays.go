7579a5030111e6d41fb5c96241f6cbc4cb79c4fa
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-15 11:25:26 +0700

    Slice copy example

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var arr = make([]int64, 10)
-	fmt.Println(arr)
+	var arr1 = []int64{1, 2, 3}
+	fmt.Println(arr1)
+	arr1 = append(arr1, 4, 5, 6, 7)
+	fmt.Println(arr1)
+
+	var arr2 = make([]int64, 3)
+	copy(arr2, arr1)
+	fmt.Println(arr2)
 }
