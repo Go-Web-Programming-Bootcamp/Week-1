8c8dc79eb297997ab78ea28cbd236fd269ce1d6a
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-15 11:09:36 +0700

    Slice example

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var arr [5]int
-	arr[0] = 5
-	arr[1] = 10
-	arr[2] = 17
-	arr[3] = 20
-	arr[4] = 25
+	var arr = []int{2, 5, 10, 11}
 
 	var total int
 	for _, value := range arr {
