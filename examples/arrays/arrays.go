c6367a294efe85c1ef151422869b6e8c0c20b8c3
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-15 10:55:48 +0700

    More complicated arrays example

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -1,11 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var arr [5]int
-	arr[4] = 100
-	fmt.Println(arr)
+	arr[0] = 5
+	arr[1] = 10
+	arr[2] = 17
+	arr[3] = 20
+	arr[4] = 25
+
+	var total int
+	for _, value := range arr {
+		total += value
+	}
+
+	fmt.Println(float64(total) / float64(len(arr)))
 }
