3fd80df119213e228f57aa19bb03cd43f2ee755d
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-07-15 11:14:34 +0700

    Slice using Make

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var arr = []int{2, 5, 10, 11}
-
-	var total int
-	for _, value := range arr {
-		total += value
-	}
-
-	fmt.Println(float64(total) / float64(len(arr)))
+	var arr = make([]int64, 10)
+	fmt.Println(arr)
 }
